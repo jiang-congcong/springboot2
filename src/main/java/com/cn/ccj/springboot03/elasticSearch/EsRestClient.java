733d31ba76a81d12aab74ddf8942f190917f6e7f
jiangcongcong <1912758962@qq.com>
2021-03-15 20:06:58 +0800

    springboot2项目整合elasticSearch restHighLevelClient查询+插入功能

--- src/main/java/com/cn/ccj/springboot03/elasticSearch/EsRestClient.java
+++ src/main/java/com/cn/ccj/springboot03/elasticSearch/EsRestClient.java
@@ -1,15 +1,28 @@
 package com.cn.ccj.springboot03.elasticSearch;
 
+import com.cn.ccj.springboot03.generalException.GeneralException;
+import com.cn.ccj.springboot03.jsonUtil.JsonUtil;
+import com.cn.ccj.springboot03.redis.RedisOperate;
+import com.cn.ccj.springboot03.util.CommonCode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.netty.util.internal.StringUtil;
 import org.apache.lucene.search.TotalHits;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.eql.EqlSearchResponse;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -28,6 +41,10 @@ import java.util.Map;
 public class EsRestClient {
 
     private RestHighLevelClient restHighLevelClient;
+
+    public static Logger logger = LoggerFactory.getLogger(RedisOperate.class);
+
+    private CommonCode commonCode;
 
     public EsRestClient() {
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
@@ -56,6 +73,76 @@ public class EsRestClient {
         }
         resultMap.put("total",total);
         resultMap.put("meaasge",getHits);
+        return resultMap;
+    }
+
+    //单条数据插入ES
+    public void putMessageToES(String index,String type,Object putMessage) throws Exception {
+        if(null==index||null==type||index.length()<1||type.length()<1){
+            throw new GeneralException(commonCode.DEFAUT_ERROR_CODE,"ES索引和类型必传不为空");
+        }
+        if(null==putMessage){
+            throw new GeneralException(commonCode.DEFAUT_ERROR_CODE,"插入ES数据不能为空");
+        }
+        IndexRequest indexRequest = new IndexRequest(index, type);
+        String source = JsonUtil.objectToString(putMessage);//转成json字符串
+        indexRequest.source(source, XContentType.JSON);
+        try {
+            restHighLevelClient.index(indexRequest,RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            logger.error(putMessage+"插入ES失败："+e.getMessage());
+            throw new GeneralException(commonCode.DEFAUT_ERROR_CODE,"插入ES失败");
+        }
+
+    }
+
+    //批量数据插入ES
+    public void batchPutMessageToES(String index,String type,List batchPutMessage) throws Exception {
+        if(null==index||null==type||index.length()<1||type.length()<1){
+            throw new GeneralException(commonCode.DEFAUT_ERROR_CODE,"ES索引和类型必传不为空");
+        }
+        if(null==batchPutMessage){
+            throw new GeneralException(commonCode.DEFAUT_ERROR_CODE,"批量插入ES数据不能为空");
+        }
+        BulkRequest bulkRequest = new BulkRequest();
+        for(Object eachbatchPutMessage:batchPutMessage){
+            String source = JsonUtil.objectToString(eachbatchPutMessage);//转json
+            IndexRequest indexRequest = new IndexRequest(index,type);
+            indexRequest.source(source,XContentType.JSON);
+            bulkRequest.add(indexRequest);
+        }
+        try {
+            restHighLevelClient.bulk(bulkRequest,RequestOptions.DEFAULT);
+        }catch (Exception e){
+            logger.error(batchPutMessage+"批量插入ES失败："+e.getMessage());
+            throw new GeneralException(commonCode.DEFAUT_ERROR_CODE,"批量插入ES失败");
+        }
+
+    }
+
+    //查询ES数据
+    public Map<String,Object> queryMessageFromES(String index,SearchSourceBuilder searchSourceBuilder) throws Exception {
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        SearchRequest searchRequest = new SearchRequest(index);
+        searchRequest.source(searchSourceBuilder);
+        try{
+            SearchResponse searchResponce = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+            Long total = searchResponce.getHits().getTotalHits().value;
+            resultMap.put("total",total);
+            SearchHit[] hits = searchResponce.getHits().getHits();
+            List<Map<String,Object>> resultList = new ArrayList<Map<String,Object>>();
+            for(SearchHit hit:hits){
+                Map<String,Object> eachMap = hit.getSourceAsMap();
+                resultList.add(eachMap);
+            }
+            resultMap.put("resultList",resultList);
+        }catch (Exception e){
+            logger.error(searchSourceBuilder+"查询ES失败："+e.getMessage());
+            throw new GeneralException(commonCode.DEFAUT_ERROR_CODE,"查询ES失败");
+        }
+
+
         return resultMap;
     }
 
--- src/test/java/com/cn/ccj/springboot03/Springboot03ApplicationTests.java
+++ src/test/java/com/cn/ccj/springboot03/Springboot03ApplicationTests.java
@@ -2,8 +2,15 @@ package com.cn.ccj.springboot03;
 
 import com.cn.ccj.springboot03.elasticSearch.EsRestClient;
 import com.cn.ccj.springboot03.entity.User;
+import com.cn.ccj.springboot03.generalException.GeneralException;
+import com.cn.ccj.springboot03.jsonUtil.JsonUtil;
 import com.cn.ccj.springboot03.redis.RedisOperate;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisOperations;
@@ -25,6 +32,8 @@ class Springboot03ApplicationTests {
 
     @Autowired
     EsRestClient esRestClient;
+
+    public static Logger logger = LoggerFactory.getLogger(RedisOperate.class);
 
     @Test
     public void setUserNameToRedis(){
@@ -70,6 +79,70 @@ class Springboot03ApplicationTests {
     public void testEsRestClient() throws IOException {
         Map map = esRestClient.searchEsMessageAll();
         System.out.println(map);
+    }
+
+    @Test
+    public void testEsRestClient01() throws Exception { //测试es单条插入
+        String index = "test";
+        String type = "";
+        Map map = new HashMap();
+        map.put("name","小花");
+        map.put("age",18);
+        map.put("sex","女");
+        try {
+            esRestClient.putMessageToES(index,type,map);
+        }catch (Exception e){
+            logger.error("插入ES失败");
+            throw new GeneralException("-9999","插入ES失败");
+        }
+        System.out.println(map);
+    }
+
+    @Test
+    public void testEsRestClient02() throws Exception { //测试es批量插入
+        String index = "test";
+        String type = "_doc";
+        Map map1 = new HashMap();
+        map1.put("name","小红");
+        map1.put("age",18);
+        map1.put("sex","女");
+        map1.put("hobby","看书，下棋，玩手机，打游戏");
+        Map map2 = new HashMap();
+        map2.put("name","小张");
+        map2.put("age",20);
+        map2.put("sex","男");
+        map2.put("hobby","玩手机，打游戏，刷视频");
+        List list = new ArrayList();
+        list.add(map1);
+        list.add(map2);
+        try {
+            esRestClient.batchPutMessageToES(index,type,list);
+        }catch (Exception e){
+            logger.error("插入ES失败");
+            throw new GeneralException("-9999","插入ES失败");
+        }
+
+    }
+
+    @Test
+    public void testEsRestClient03() throws Exception { //测试es查询功能
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.from(0);
+        searchSourceBuilder.size(10);
+        searchSourceBuilder.sort("age");
+        //key.keyword key+.keyword来匹配，不然字符串匹配不上，IK分词器的问题（elasticsearch 里默认的IK分词器是会将每一个中文都进行了分词的切割，所以你直接想查一整个词，或者一整句话是无返回结果的）
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name.keyword","小红");
+        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("hobby","打游戏");
+        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("age");
+        rangeQueryBuilder.gte(12);
+        rangeQueryBuilder.lte(30);
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(termQueryBuilder);
+        boolQueryBuilder.must(matchQueryBuilder);
+        boolQueryBuilder.must(rangeQueryBuilder);
+        searchSourceBuilder.query(boolQueryBuilder);
+        Map<String,Object> resultMap = esRestClient.queryMessageFromES("test",searchSourceBuilder);
+        System.out.println(resultMap);
     }
 
 }
