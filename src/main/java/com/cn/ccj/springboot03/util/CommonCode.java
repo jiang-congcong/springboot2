97dc904c9f6ebdef6a57fc0232228b268f9294a0
jiangcongcong <1912758962@qq.com>
2021-03-16 11:08:04 +0800

    springboot2项目封装公共编码类

---
+++ src/main/java/com/cn/ccj/springboot03/util/CommonCode.java
@@ -0,0 +1,13 @@
+package com.cn.ccj.springboot03.util;
+
+/**
+ * @author jiangcongcong
+ * @date 2021/3/15 16:05
+ * 公共编码类
+ */
+public class CommonCode {
+
+    //默认异常编码
+    public String DEFAUT_ERROR_CODE = "-9999";
+
+}
